8188dcc7e75cce9dbba364eb1397ef9e357ab655
republicista13 <republicista@gmail.com>
2021-10-26 15:07:44 -0300

    phpmailer-final

--- envio.php
+++ envio.php
@@ -6,9 +6,10 @@ require("class.smtp.php");
 if ( !isset($_POST["nombre"]) || !isset($_POST["email"]) || !isset($_POST["tel"]) || !isset($_POST["mensaje"]) ) {
     die ("Es necesario completar todos los datos del formulario");
 }
+$destino = "info@republicista.ar";
 $nombre = $_POST["nombre"];
 $email = $_POST["email"];
-$email = $_POST["tel"];
+$tel = $_POST["tel"];
 $mensaje = $_POST["mensaje"];
 
 // Datos de la cuenta de correo utilizada para enviar vía SMTP
@@ -17,7 +18,7 @@ $smtpUsuario = "test@republicista.ar";  // Mi cuenta de correo
 $smtpClave = "casa-5837";  // Mi contraseña
 
 // Email donde se enviaran los datos cargados en el formulario de contacto
-$emailDestino = "info@republicista.com.ar";
+$emailDestino = "info@republicista.ar";
 
 $mail = new PHPMailer();
 $mail->IsSMTP();
@@ -39,9 +40,28 @@ $mail->AddAddress($emailDestino); // Esta es la dirección a donde enviamos los 
 
 $mail->Subject = "Formulario de Contacto Web Republicista"; // Este es el titulo del email.
 $mensajeHtml = nl2br($mensaje);
-$mail->Body = "{$mensajeHtml} <br /><br />Datos recogidos por el Formulario<br />"; // Texto del email en formato HTML
-$mail->AltBody = "{$mensaje} \n\n Formulario"; // Texto sin formato HTML
+$mail->Body = "
+<html>
+<body>
+<h1>Has recibido un mensaje desde el formulario web, estos son los datos:</h1>
+<p>Información enviada por el usuario de la web republicista.ar</p>
+<p>Nombre: {$nombre}</p>
+<p>E-mail: {$email}</p>
+<p>Teléfono: {$tel}</p>
+<p>Mensaje: {$mensaje}</p>
+</body>
+</html>
+<br />"; // Texto del email en formato HTML
+$mail->AltBody = "{$mensaje} \n\n"; // Texto sin formato HTML
 // FIN - VALORES A MODIFICAR //
+
+$mail->SMTPOptions = array(
+    'ssl' => array ( 
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true,
+    )
+    );
 
 $estadoEnvio = $mail->Send(); 
 if($estadoEnvio){
@@ -49,5 +69,4 @@ if($estadoEnvio){
 } else {
     echo "Ocurrió un error inesperado.";
 }
-   
 ?>
